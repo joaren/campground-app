f2825829e493e3558363d86ff64c67773d826a77
Jolie Ren <116987808+malonylCoA@users.noreply.github.com>
2024-01-24 13:55:14 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-const { campgroundSchema, reviewSchema} = require('./schemas.js'); //joi schemas
+const { campgroundSchema, reviewSchema} = require('./schemas.js'); 
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session')
 const flash = require('connect-flash');
@@ -9,7 +9,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     console.log('req,user...', req.user);
     if(!req.isAuthenticated()){ //from passport
         req.session.returnTo = req.originalUrl;
-        req.flash('error', 'you must be signed in first!'); //need to be logged in before accessing new campgrounds page
+        req.flash('error', 'you must be signed in first!'); 
         return res.redirect('/login')
     }
     next();
@@ -23,11 +23,11 @@ module.exports.storeReturnTo = (req, res, next) => {
 }
 
 module.exports.validateCampground = (req, res, next) => { //joi as middleware
-    const {error} = campgroundSchema.validate(req.body); //passes data thru to the schema
+    const {error} = campgroundSchema.validate(req.body); 
     if(error){
         const msg = error.details.map(el => el.message)
-        const result = messages.join(',') //map and join all the error messages
-        throw new ExpressError(result, 400) //print the above out
+        const result = messages.join(',') 
+        throw new ExpressError(result, 400) 
     } else {
         next(); //we all good
     }
@@ -35,30 +35,30 @@ module.exports.validateCampground = (req, res, next) => { //joi as middleware
 
 module.exports.isAuthor = async (req, res, next) => {
     const {id} = req.params;
-    const campground = await Campground.findById(id); //find campground
+    const campground = await Campground.findById(id); 
     if(!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!'); //cannot update if not the user 
+        req.flash('error', 'You do not have permission to do that!'); 
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
 
-module.exports.validateReview =(req, res, next) => { //server side -- make sure ppl cant bypass the error with POSTMAN and add to app.post
+module.exports.validateReview =(req, res, next) => { 
     const {error} = reviewSchema.validate(req.body)
     if(error){
         const msg = error.details.map(el => el.message)
-        const result = messages.join(',') //map and join all the error messages
-        throw new ExpressError(result, 400) //print the above out
+        const result = messages.join(',') 
+        throw new ExpressError(result, 400) 
     } else {
         next();
     }
 }
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const {id, reviewId} = req.params; //reviewId is from /:reviewId
-    const review = await Review.findById(reviewId); //find campground
+    const {id, reviewId} = req.params; 
+    const review = await Review.findById(reviewId); 
     if(!review.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!'); //cannot update if not the user 
+        req.flash('error', 'You do not have permission to do that!'); /
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
